eb6834531680027d79696c04cf7a12b40debbfcb
helpinfo <courrier@helpinfo.fr>
2021-04-27 16:22:07 +0200

    ajout PageModuleController.php pour séparer les controller

--- src/Controller/ModulesController.php
+++ src/Controller/ModulesController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Modules;
-use App\Entity\PageModule;
-use App\Repository\PageModuleRepository;
 use App\Repository\ModulesRepository;
 
 class ModulesController extends AbstractController
@@ -27,24 +25,6 @@ class ModulesController extends AbstractController
         ]);
     }
 
-
-    /**
-     *  @Route("/contenu/{id}", name="contenu")
-     */
-    public function contenu($id): Response
-     //public function contenu(Modules $module, PageModule $pageModule)
-    {
-        $repoM = $this->getDoctrine()->getRepository(Modules::class);
-        $repoP = $this->getDoctrine()->getRepository(PageModule::class);
-
-        $module = $repoM->find($id);
-        $pageModule = $repoP->findBy([],['modules' => 'asc']);
-
-            return $this->render('modules/contenu.html.twig', [
-                'module' => $module,
-                'contenu' => $pageModule
-        ]);
-        }
 
 
     /**
